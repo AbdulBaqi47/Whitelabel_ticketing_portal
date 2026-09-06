e2ea3794d1f414f13459d3c1716a5cfd489f4a14
Abdul Baqi <74543895+AbdulBaqi47@users.noreply.github.com>
2026-09-06 13:21:11 +0500

    Fix CORS_ALLOWED_ORIGINS URL typo

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins' => array_filter(array_map(
         'trim',
-        explode(',', env('CORS_ALLOWED_ORIGINS', env('APP_FRONTEND_URL', 'https://b2bwhitelabelota.vercel.app')))
+        explode(',', env('CORS_ALLOWED_ORIGINS', env('APP_FRONTEND_URL', 'https:/b2bwhitelabelota.vercel.app')))
     )),
     
     'allowed_headers' => ['*'],
